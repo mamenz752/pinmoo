e259574ba289bd1d5a0997e05f9ca45465e85eb3
mamenz752 <mamenz752@gmail.com>
2024-05-08 22:42:05 +0900

    feat: usersテーブルの変更

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken('created_at');
+            $table->timestamps('updated_at');
+            $table->string('image_path');
         });
     }
 
