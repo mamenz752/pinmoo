ccd2c23d702d7f96d16d88615c34fd48eb0a0ba8
mamenz752 <mamenz752@gmail.com>
2024-06-10 20:57:35 +0900

    fix: クイックステータスのUI修正

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -17,10 +17,17 @@ export default function Dashboard(props) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="py-6 flex items-center justify-center gap-2">
+                            <svg width="4rem" height="2rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.5 11.5H14.5L13 14.5L11 8.5L9.5 11.5H8.5M11.9932 5.13581C9.9938 2.7984 6.65975 2.16964 4.15469 4.31001C1.64964 6.45038 1.29697 10.029 3.2642 12.5604C4.75009 14.4724 8.97129 18.311 10.948 20.0749C11.3114 20.3991 11.4931 20.5613 11.7058 20.6251C11.8905 20.6805 12.0958 20.6805 12.2805 20.6251C12.4932 20.5613 12.6749 20.3991 13.0383 20.0749C15.015 18.311 19.2362 14.4724 20.7221 12.5604C22.6893 10.029 22.3797 6.42787 19.8316 4.31001C17.2835 2.19216 13.9925 2.7984 11.9932 5.13581Z" stroke="#FF2D2D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <p className="text-lg">今の気分をシェアしましょう</p>
+                        </div>
                         <form
-                            className="grid gap-4"
                         >
-                            <div>
+                            <div
+                                className="mx-auto py-6 w-[60%] grid items-center justify-center grid-cols-5 gap-2"
+                            >
                                 { moods.map((mood) => (
                                     <button
                                         key={mood.id}
@@ -31,9 +38,6 @@ export default function Dashboard(props) {
                                 )) }
                             </div>
                         </form>
-                        
-                        
-                        <div className="p-6 text-gray-900">You're logged in!</div>
                     </div>
                 </div>
             </div>
