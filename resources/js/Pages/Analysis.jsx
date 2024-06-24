c43b4b8beea77825f76dfc5c9ea5f4591ee7210a
mamenz752 <mamenz752@gmail.com>
2024-06-24 23:11:59 +0900

    feat: 感情レーダーチャートの表示

--- resources/js/Pages/Analysis.jsx
+++ resources/js/Pages/Analysis.jsx
@@ -1,4 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import MoodRaderChart from '@/Pages/Analysis/MoodRaderChart';
 import { Head } from '@inertiajs/react';
 
 export default function Analysis(props) {
@@ -6,14 +7,18 @@ export default function Analysis(props) {
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Analysis</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">感情グラフ</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="感情グラフ" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        
+                        <h1>ここ3日間の自分の気分</h1>
+                        
+                        <MoodRaderChart />
+                
                     </div>
                 </div>
             </div>
