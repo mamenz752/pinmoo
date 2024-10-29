52f0fc0706e9f7f155e6d067c0b3d099f61d10e0
mamenz752 <mamenz752@gmail.com>
2024-10-29 11:01:42 +0900

    feat: MoodsとStatusesについてSeederを実施

---
+++ database/seeders/MoodsTableSeeder.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MoodsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('moods')->insert([
+            'feeling' => 'angry',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/angry.png',
+            'category' => 'NEGATIVE'
+        ]);
+        DB::table('moods')->insert([
+            'feeling' => 'sad',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/sad.png',
+            'category' => 'NEGATIVE'
+        ]);
+        DB::table('moods')->insert([
+            'feeling' => 'scared',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/scared.png',
+            'category' => 'NEGATIVE'
+        ]);
+        DB::table('moods')->insert([
+            'feeling' => 'nervous',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/nervous.png',
+            'category' => 'NEGATIVE'
+        ]);
+        DB::table('moods')->insert([
+            'feeling' => 'smile',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/smile.png',
+            'category' => 'NORMAL'
+        ]);
+        DB::table('moods')->insert([
+            'feeling' => 'surprised',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/surprised.png',
+            'category' => 'NORMAL'
+        ]);
+        DB::table('moods')->insert([
+            'feeling' => 'wink',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/wink.png',
+            'category' => 'POSITIVE'
+        ]);
+        DB::table('moods')->insert([
+            'feeling' => 'joyful',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/joyful.png',
+            'category' => 'POSITIVE'
+        ]);
+        DB::table('moods')->insert([
+            'feeling' => 'stared',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/stared.png',
+            'category' => 'POSITIVE'
+        ]);
+        DB::table('moods')->insert([
+            'feeling' => 'love',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715698852/pinmoo/moods/love.png',
+            'category' => 'POSITIVE'
+        ]);
+    }
+}
---
+++ database/seeders/StatusesTableSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('statuses')->insert([
+            'status' => 'friends',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/friends.png'
+        ]);
+        DB::table('statuses')->insert([
+            'status' => 'family',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/family.png'
+        ]);
+        DB::table('statuses')->insert([
+            'status' => 'lover',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/lover.png'
+        ]);
+        DB::table('statuses')->insert([
+            'status' => 'sunny',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/sunny.png'
+        ]);
+        DB::table('statuses')->insert([
+            'status' => 'cloudy',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/cloudy.png'
+        ]);
+        DB::table('statuses')->insert([
+            'status' => 'rainy',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/rainy.png'
+        ]);
+        DB::table('statuses')->insert([
+            'status' => 'study',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/study.png'
+        ]);
+        DB::table('statuses')->insert([
+            'status' => 'exam',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/exam.png'
+        ]);
+        DB::table('statuses')->insert([
+            'status' => 'project',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/project.png'
+        ]);
+        DB::table('statuses')->insert([
+            'status' => 'work',
+            'image_path' => 'https://res.cloudinary.com/drzq0yhkr/image/upload/v1715772974/pinmoo/statuses/work.png'
+        ]);
+    }
+}
