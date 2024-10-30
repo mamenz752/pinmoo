ec23813f20506572255361b4a0286f168f6e6d06
mamenz752 <mamenz752@gmail.com>
2024-10-30 20:35:51 +0900

    fix: カラーパレットの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,15 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'pi-red': '#C70100',
+                'pi-orange': "#FF9304",
+                'pi-yellow': '#EDE700',
+                'pi-green': '#0DB85C',
+                'pi-blue': '#0E89FD',
+                'pi-dark-blue': "#07008C",
+                'pi-black': "#333333",
+            }
         },
     },
 
