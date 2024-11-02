fde644c5b1805b6bdef0d6124dfd3344b0d46e4a
mamenz752 <mamenz752@gmail.com>
2024-11-02 12:32:36 +0900

    update: ModalのUI完成

--- resources/js/Pages/Status/QuickStatusModal.jsx
+++ resources/js/Pages/Status/QuickStatusModal.jsx
@@ -1,14 +1,22 @@
+import { AdvancedImage } from '@cloudinary/react';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react'
+import { Cloudinary } from '@cloudinary/url-gen';
 
 const QuickStatusModal = (props) => {
     const { showQuickStatusModal, onShowQuickStatusModalFn } = props;
     const [moods, setMoods] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    
+    const cld = new Cloudinary({
+        cloud: {
+          cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
+        },
+      });
 
     useEffect(() => {
         const getAllMoods = () => {
-            axios.get(`http://localhost:80/api/v1/get-all-moods/`)
+            axios.get(`${import.meta.env.VITE_API_URL}/api/v1/get-all-moods/`)
             .then((res) => {
                 console.log(res.data.moods);
                 setMoods(res.data.moods);
@@ -33,34 +41,43 @@ const QuickStatusModal = (props) => {
                 className="z-[30] max-h-full"
             >
                 {/* Modal Body */}
-                <div className='mx-auto absolute left-1/2 -translate-x-1/2 bottom-32 p-4 bg-white rounded-md'>
+                <div
+                    className='z-[40] mx-auto absolute left-1/2 -translate-x-1/2 bottom-32 p-4 bg-white rounded-md'
+                    onClick={(e) => e.stopPropagation()}
+                >
                     <div>
                         <p>今の気分をシェアしましょう</p>
                     </div>
                     <form
                         onSubmit={(e) => e.preventDefault()}
                     >
-                        { isLoading ?
-                            <></>
-                            :
-                            // <></>
-                            moods.map((mood, i) => {
-                                <>
-                                    <input
-                                        id={mood.feeling}
-                                        type='radio'
-                                        name='mood'
-                                        value={mood.feeling}
-                                    />
-                                    <label
-                                        key={i}
-                                        for={mood.feeling}
-                                    >
-                                        <img src={mood.image_path} />
-                                    </label>
-                                </>
-                            })
-                        }
+                        <div className='my-4 grid grid-cols-5 gap-2'>
+                            { isLoading ?
+                                <></>
+                                :
+                                moods.map((mood, i) => {
+                                    const myImg = cld.image(mood.image_path);
+                                    return (
+                                    <div className='flex flex-col items-center justify-center'>
+                                        <input
+                                            id={mood.feeling}
+                                            type='radio'
+                                            name='mood'
+                                            value={mood.feeling}
+                                        />
+                                        <label
+                                            key={i}
+                                            for={mood.feeling}
+                                        >
+                                            <div className='w-20 h-full'>
+                                                <AdvancedImage cldImg={myImg} />
+                                            </div>
+                                        </label>
+                                    </div>
+                                    )
+                                })
+                            }
+                        </div>
                         <button
                             type='submit'
                             onClick={onShowQuickStatusModalFn}
