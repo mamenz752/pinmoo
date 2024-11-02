beb650b597cf35fffdb47637f424a34eac08f989
mamenz752 <mamenz752@gmail.com>
2024-11-03 00:48:37 +0900

    update: 共通UIのHeadやHeaderの実装

--- resources/js/Pages/Analysis/AnalysisPresenter.jsx
+++ resources/js/Pages/Analysis/AnalysisPresenter.jsx
@@ -1,9 +1,17 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
 
 const AnalysisPresenter = () => {
   return (
-    <AuthenticatedLayout>
-      <p>Analysis</p>
+    <AuthenticatedLayout
+      header={"Analysis"}
+    >
+        <Head title="Analysis" />
+      <div className="my-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="p-4 overflow-hidden bg-white shadow-sm sm:rounded-lg">
+              <p>Coming soon...</p>
+          </div>
+      </div>
     </AuthenticatedLayout>
   )
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -12,13 +12,9 @@ export default function Dashboard({moods, newPost}) {
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            <p>A</p>
-                        </div>
-                    </div>
+            <div className="my-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="p-4 overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    a
                 </div>
             </div>
         </AuthenticatedLayout>
