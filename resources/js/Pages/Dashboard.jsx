9c2e48ba31f8dab1f00e10de66c127e06d998795
mamenz752 <mamenz752@gmail.com>
2024-11-05 23:16:25 +0900

    feat: ともだちの気分閲覧機能の実装

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -3,11 +3,15 @@ import PostEditIcon from '@/Icons/PostEditIcon';
 import { Head, Link } from '@inertiajs/react';
 import { useEffect, useState } from 'react';
 
-export default function Dashboard({user, moods, newPost}) {
+export default function Dashboard({user, moods, newPost, friends, friendsPosts}) {
     const [currentMood, setCurrentMood] = useState();
+    const [friendsMood, setFriendsMood] = useState();
     useEffect(() => {
         if (newPost) {
             setCurrentMood(moods.filter(mood => mood.id === newPost.mood_id)[0]);
+        }
+        if (friendsPosts) {
+            setFriendsMood(friendsPosts.map(post => moods.filter(mood => mood.id === post.mood_id)[0]));
         }
     }, [])
 
@@ -63,35 +67,45 @@ export default function Dashboard({user, moods, newPost}) {
 
             <div className="my-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <h1 className='mt-4 text-xl tracking-wider font-bold'>ともだちの気分</h1>
-                <div className="mt-4 p-4 flex justify-between items-center bg-white shadow-sm sm:rounded-lg">
-                    <p>{user.username}</p>
-                    <div className='flex gap-4 items-center'>
-                        <div>
-                            <p className='text-lg tracking-wider'>
-                                {newPost ? newPost.comment :<></>}
-                            </p>
-                        </div>
-                        <div className='w-12 h-12'>
-                            {
-                                newPost && currentMood ?
-                                    <img src={currentMood.image_path} alt={currentMood.feeling} />
-                                : <></>
-                            }
-                        </div>
-                        <div>
-                            {
-                                newPost ?
-                                    <Link
-                                        href={route('posts.edit', newPost.id)}
-                                        className='text-gray-600 hover:opacity-80'
-                                    >
-                                        <PostEditIcon />
-                                    </Link>
-                                :   <></>
-                            }
+                {
+                    friendsPosts.length ?
+                    friendsPosts.map((post, i) => {
+                        const user = friends.filter(friend => friend.id === post.user_id)[0];
+                        return (
+                    <div className="mt-4 p-4 flex justify-between items-center bg-white shadow-sm sm:rounded-lg">
+                        <p>{user.username}</p>
+                        <div className='flex gap-4 items-center'>
+                            <div>
+                                <p className='text-lg tracking-wider'>
+                                    {post ? post.comment :<></>}
+                                </p>
+                            </div>
+                            <div className='w-12 h-12'>
+                                {
+                                    post && friendsMood ?
+                                        <img src={friendsMood[i].image_path} alt={friendsMood[i].feeling} />
+                                    : <></>
+                                }
+                            </div>
+                            <div>
+                                {
+                                    post ?
+                                        <Link
+                                            href={route('dashboard')}
+                                            className='text-pi-red hover:opacity-80'
+                                        >
+                                            <PostEditIcon />
+                                        </Link>
+                                    :   <></>
+                                }
+                            </div>
                         </div>
                     </div>
-                </div>
+                        )
+                    })
+                    :
+                    <></>
+                }
             </div>
         </AuthenticatedLayout>
     );
