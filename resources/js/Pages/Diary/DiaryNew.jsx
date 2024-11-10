2d57992137886a0e7224c0ed094ccdcbb20e2cf1
mamenz752 <mamenz752@gmail.com>
2024-11-10 17:48:59 +0900

    fix: 感情リレーション機能のtype=radio修正

--- resources/js/Pages/Diary/DiaryNew.jsx
+++ resources/js/Pages/Diary/DiaryNew.jsx
@@ -142,7 +142,7 @@ const DiaryNew = ({ posts, moods }) => {
                                     >
                                         <div className='w-full flex items-center gap-2'>
                                             <input
-                                                type="checkbox"
+                                                type="radio"
                                                 id={post.id}
                                                 className="w-4 h-4 vertical-align-middle"
                                                 name="post_id"
